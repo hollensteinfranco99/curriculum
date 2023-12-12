28c54f3b032468cd45b5a027b55fc102d5660599
fhollenstein <hollensteinfranco99@gmail.com>
2023-12-12 18:10:56 -0300

    se modifica proyecto

--- src/components/ContenedorSecciones.jsx
+++ src/components/ContenedorSecciones.jsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useLayoutEffect,useState, useEffect, useRef } from 'react';
+import React, { forwardRef, useLayoutEffect, useState, useEffect } from 'react';
 import Inicio from './Inicio';
 import Proyectos from './Proyectos';
 import Contacto from './Contacto';
@@ -6,8 +6,9 @@ import Skills from './Skills';
 import SobreMi from './SobreMi';
 
 const ContenedorSecciones = (props) => {
-
-    const [esDispositivoPequeno, setEsDispositivoPequeno] = useState(window.innerWidth <= 900);
+    const [esDispositivoPequeno, setEsDispositivoPequeno] = useState(
+        window.innerWidth <= 900
+    );
 
     useLayoutEffect(() => {
         const handleResize = () => {
@@ -20,57 +21,76 @@ const ContenedorSecciones = (props) => {
             window.removeEventListener('resize', handleResize);
         };
     }, []);
+
     useEffect(() => {
+        const scrollear = () => {
+            const container = document.querySelector('.scroll-container');
+
+            let isTouching = false;
+            let startTouchY = 0;
+
+            const handleTouchStart = (event) => {
+                event.preventDefault();
+                isTouching = true;
+                startTouchY = event.touches[0].clientY;
+            };
+
+            const handleTouchMove = (event) => {
+                if (!isTouching) return;
+                event.preventDefault();
+
+                const currentTouchY = event.touches[0].clientY;
+                const delta = startTouchY - currentTouchY;
+
+                container.scrollBy({
+                    top: delta,
+                    behavior: 'smooth',
+                });
+
+                startTouchY = currentTouchY;
+            };
+
+            const handleTouchEnd = () => {
+                isTouching = false;
+            };
+
+            const handleWheel = (event) => {
+                event.preventDefault();
+                const delta = event.deltaY;
+
+                container.scrollBy({
+                    top: delta,
+                    behavior: 'smooth',
+                });
+            };
+
+            container.addEventListener('touchstart', handleTouchStart);
+            container.addEventListener('touchmove', handleTouchMove);
+            container.addEventListener('touchend', handleTouchEnd);
+            container.addEventListener('wheel', handleWheel);
+
+            return () => {
+                container.removeEventListener('touchstart', handleTouchStart);
+                container.removeEventListener('touchmove', handleTouchMove);
+                container.removeEventListener('touchend', handleTouchEnd);
+                container.removeEventListener('wheel', handleWheel);
+            };
+        };
+
         scrollear();
-    }, [])
-
-
-    const scrollear = () => {
-        const container = document.querySelector('.scroll-container');
-
-        let isTouching = false;
-        let startTouchY = 0;
-
-        container.addEventListener('touchstart', (event) => {
-            isTouching = true;
-            startTouchY = event.touches[0].clientY;
-        });
-
-        container.addEventListener('touchmove', (event) => {
-            if (!isTouching) return;
-
-            const currentTouchY = event.touches[0].clientY;
-            const delta = startTouchY - currentTouchY;
-
-            container.scrollBy({
-                top: delta,
-                behavior: 'smooth'
-            });
-
-            startTouchY = currentTouchY;
-        });
-
-        container.addEventListener('touchend', () => {
-            isTouching = false;
-        });
-
-        container.addEventListener('wheel', (event) => {
-            event.preventDefault();
-            const delta = event.deltaY;
-
-            container.scrollBy({
-                top: delta,
-                behavior: 'smooth'
-            });
-        });
-    }
+    }, []);
 
     return (
         <section className='scroll-container'>
             <Inicio ref={props.refsLista.inicio}></Inicio>
-            <Skills esDispositivoPequeno={esDispositivoPequeno} ref={props.refsLista.skills}></Skills>
-            {esDispositivoPequeno ? <SobreMi ref={props.refsLista.sobremi}></SobreMi> : null}
-            
+            <Skills
+                esDispositivoPequeno={esDispositivoPequeno}
+                ref={props.refsLista.skills}
+            ></Skills>
+            {esDispositivoPequeno ? (
+                <SobreMi ref={props.refsLista.sobremi}></SobreMi>
+            ) : null}
+
             <Proyectos ref={props.refsLista.proyectos}></Proyectos>
             <Contacto ref={props.refsLista.contacto}></Contacto>
         </section>
