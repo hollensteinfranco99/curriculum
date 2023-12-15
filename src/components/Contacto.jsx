2cbd545fa8610ad4a6ac762afee6287c9f0844ad
fhollenstein <hollensteinfranco99@gmail.com>
2023-12-15 17:23:09 -0300

    se modifica

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, forwardRef, useRef } from 'react';
+import React, { useEffect, forwardRef, useRef,useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/contacto.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -11,7 +11,7 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 const Contacto = forwardRef((props, ref) => {
 
     const form = useRef();
-
+    const [elementoPdf, setElementoPdf] = useState(null);
     const sendEmail = (e) => {
         e.preventDefault();
         let asunto = document.getElementById("asunto");
@@ -24,15 +24,15 @@ const Contacto = forwardRef((props, ref) => {
         let vnombre = validarCampos(nombre);
         let vemail = validarEmail(email);
 
-        if(vasunto === true && vemail === true && vnombre === true && vmensaje === true){
+        if (vasunto === true && vemail === true && vnombre === true && vmensaje === true) {
 
-            emailjs.sendForm('service_drzhtfh', 'template_l3hbyc9', form.current, 'GOv-lfA6Qgik1eJg5').then(()=>{
+            emailjs.sendForm('service_drzhtfh', 'template_l3hbyc9', form.current, 'GOv-lfA6Qgik1eJg5').then(() => {
                 Swal.fire({
                     text: "Se envio de forma correcta su mensaje",
                     icon: "success"
-                }); 
-
-            }).error((error)=>{
+                });
+                limpiarForm();
+            }).error((error) => {
                 console.log(error);
                 Swal.fire({
                     icon: "error",
@@ -41,7 +41,7 @@ const Contacto = forwardRef((props, ref) => {
                 });
 
             });
-        }else{
+        } else {
             Swal.fire({
                 icon: "error",
                 title: "Error al enviar mensaje",
@@ -49,7 +49,7 @@ const Contacto = forwardRef((props, ref) => {
             });
         }
     };
-    const limpiarForm = () =>{
+    const limpiarForm = () => {
         form.current.reset();
         let asunto = document.getElementById("asunto");
         let email = document.getElementById("email");
@@ -61,16 +61,16 @@ const Contacto = forwardRef((props, ref) => {
         email.className = "form-control";
         nombre.className = "form-control";
     }
-    const validarCampos = (input) =>{
-        if(input.value.trim() != ''){
+    const validarCampos = (input) => {
+        if (input.value.trim() != '') {
             input.className = 'form-control is-valid';
             return true;
-        }else{
+        } else {
             input.className = 'form-control is-invalid'
             return false;
         }
     }
-    const validarEmail = () =>{
+    const validarEmail = () => {
         const emailInput = form.current.elements.email;
         const email = emailInput.value.trim();
         const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -83,53 +83,68 @@ const Contacto = forwardRef((props, ref) => {
             return false;
         }
     }
-    const validarMensaje = (input) =>{
-        if(input.value.trim() != '' && input.value.trim().length > 10 ){
+    const validarMensaje = (input) => {
+        if (input.value.trim() != '' && input.value.trim().length > 10) {
             input.className = 'form-control is-valid';
             return true;
-        }else{
+        } else {
             input.className = 'form-control is-invalid';
             return false;
         }
     }
+    const DescargarPdf = () =>{
+        const isMobileDevice = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+        const pdfUrl = '/pdf/HollensteinFrancoCV.pdf';
 
+        if (isMobileDevice) {
+            const link = document.createElement('a');
+            link.href = pdfUrl;
+            link.download = 'HollensteinFrancoCV.pdf';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } else {
+            // Abre la página en dispositivos no móviles
+            window.open('/pdf', '_blank');
+        }
+    }
     return (
         <section data-section="contacto" ref={ref} className='scroll-item contenedor-contacto'>
             <div className='titulo-contacto'>
-                <h2>Contacto</h2>
+                <h2>Contactso</h2>
                 <p>Gracias por llegar hasta aca!</p>
             </div>
             <section className='seccion-contacto row'>
                 <article className='contenedor-formulario col-lg-6 col-md-6 col-sm-12' aria-label='formulario'>
                     <form ref={form} onSubmit={sendEmail} className='formulario'>
-                    <div>
-                        <input id='nombre' name='nombre' onBlur={(e) => validarCampos(e.target)} className='form-control' placeholder='nombre' type="text"  required/>
-                        <div className='invalid-feedback'>Debe ingrese un nombre.</div>
-                    </div>
-                    <div>
-                        <input id='email' name='email' onBlur={()=>validarEmail()} className='form-control' placeholder='email' type="email" required/>
-                        <div className='invalid-feedback'>
-                        Debe ingrese un mail valido.
+                        <div>
+                            <input id='nombre' name='nombre' onBlur={(e) => validarCampos(e.target)} className='form-control' placeholder='nombre' type="text" required />
+                            <div className='invalid-feedback'>Debe ingrese un nombre.</div>
                         </div>
-                    </div>
-                    <div>
-                        <input id='asunto' name='asunto' onBlur={(e)=>validarCampos(e.target)} className='form-control' placeholder='asunto' type="text"  required/>
-                        <div className='invalid-feedback'>
-                            Debe ingresar el asunto de su contacto.
+                        <div>
+                            <input id='email' name='email' onBlur={() => validarEmail()} className='form-control' placeholder='email' type="email" required />
+                            <div className='invalid-feedback'>
+                                Debe ingrese un mail valido.
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <textarea id='mensaje' name="mensaje" onBlur={(e)=>validarMensaje(e.target)} className='form-control' placeholder='su mensaje' cols="30" rows="10" required></textarea>
-                        <div className='invalid-feedback'>
-                            Debe ingresar un mensaje mayor a 10 caracteres
+                        <div>
+                            <input id='asunto' name='asunto' onBlur={(e) => validarCampos(e.target)} className='form-control' placeholder='asunto' type="text" required />
+                            <div className='invalid-feedback'>
+                                Debe ingresar el asunto de su contacto.
+                            </div>
                         </div>
-                    </div>
+                        <div>
+                            <textarea id='mensaje' name="mensaje" onBlur={(e) => validarMensaje(e.target)} className='form-control' placeholder='su mensaje' cols="30" rows="10" required></textarea>
+                            <div className='invalid-feedback'>
+                                Debe ingresar un mensaje mayor a 10 caracteres
+                            </div>
+                        </div>
                         <button className='btn btn-success'>Enviar mensaje</button>
                     </form>
                 </article>
                 <article className='mapa col-lg-6 col-md-6 col-sm-12' aria-label='mapa'>
                     <div>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d113927.15975832954!2d-65.30499574754882!3d-26.832835032918194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94223792d6c56903%3A0xf88d5b92b5c56527!2sSan%20Miguel%20de%20Tucum%C3%A1n%2C%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1700632037750!5m2!1ses!2sar" loading="lazy"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d113927.15975832954!2d-65.30499574754882!3d-26.832835032918194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94223792d6c56903%3A0xf88d5b92b5c56527!2sSan%20Miguel%20de%20Tucum%C3%A1n%2C%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1700632037750!5m2!1ses!2sar" loading="lazy"></iframe>
                     </div>
                 </article>
             </section>
@@ -141,7 +156,7 @@ const Contacto = forwardRef((props, ref) => {
                 <a href='https://www.linkedin.com/in/franco-david-hollenstein-689646161/' target='_blank' className='btn btn-info'>
                     <FontAwesomeIcon className='me-1' icon={faLinkedin} />
                     Linkedin</a>
-                    <a className='btn btn-danger' href='./pdf' target='_blank'>
+                <a className='btn btn-danger'  onClick={()=>{DescargarPdf()}}>
                     <FontAwesomeIcon className='me-1' icon={faFilePdf} />
                     Curriculum</a>
             </section>
--- src/components/Pdf.jsx
+++ src/components/Pdf.jsx
@@ -1,42 +1,22 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import '../css/contacto.css';
 
-class Pdf extends Component {
-    
-    componentDidMount() {
+const Pdf = (props) => {
 
-        const isMobileDevice = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-        if (isMobileDevice) {
-            document.getElementById("enlace-pdf").click();
-            window.close();
-        }
-    }
-
-    render() {
-        return (
-            <div className='pdf'>
-            <object
-                data={require('../pdf/HollensteinFrancoCV.pdf')}
-                type="application/pdf"
-                width="100%"
-                height="100%"
-            >
-                <br />
-                <a
-                    href={require('../pdf/HollensteinFrancoCV.pdf')}
-                    id='enlace-pdf'
-                    download="HollensteinFranco-CV.pdf"
-                >
-                    Tu dispositivo no puede visualizar los PDF, da click aquí para descargarlo
-                </a>
-            </object>
-        </div>
-        );
-    }
-}
+    return (
+        <div className='pdf'>
+        <object
+            data={require('../pdf/HollensteinFrancoCV.pdf')}
+            type="application/pdf"
+            width="100%"
+            height="100%"
+        >
+            <br />
+        </object>
+    </div>
+    );
+};
 
 export default Pdf;
-
-
 
