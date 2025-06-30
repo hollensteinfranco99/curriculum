684b8cb6cb750d16aa9e653b918b55fd4b9894e9
Franco Hollenstein <hollensteinfranco99@gmail.com>
2025-06-30 00:04:37 -0300

    problema con scroll solucionado IOS

--- src/components/ContenedorSecciones.jsx
+++ src/components/ContenedorSecciones.jsx
@@ -1,100 +1,108 @@
-import React, { useLayoutEffect, useState, useEffect } from 'react';
-import Inicio from './Inicio';
-import Proyectos from './Proyectos';
-import Contacto from './Contacto';
-import Skills from './Skills';
-import SobreMi from './SobreMi';
+import React, { useLayoutEffect, useState, useEffect } from "react";
+import Inicio from "./Inicio";
+import Proyectos from "./Proyectos";
+import Contacto from "./Contacto";
+import Skills from "./Skills";
+import SobreMi from "./SobreMi";
 
 const ContenedorSecciones = (props) => {
-    const [esDispositivoPequeno, setEsDispositivoPequeno] = useState(
-        window.innerWidth <= 900
-    );
+  const [esDispositivoPequeno, setEsDispositivoPequeno] = useState(
+    window.innerWidth <= 900
+  );
 
-    useLayoutEffect(() => {
-        const handleResize = () => {
-            setEsDispositivoPequeno(window.innerWidth <= 900);
-        };
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      setEsDispositivoPequeno(window.innerWidth <= 900);
+    };
 
-        window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-    useEffect(() => {
-        const scrollear = () => {
-            const container = document.querySelector('.scroll-container');
-    
-            let isTouching = false;
-            let startTouchY = 0;
-            const sensitivity = 1; // Ajusta la sensibilidad según tus necesidades
-    
-            const handleTouchStart = (event) => {
-                isTouching = true;
-                startTouchY = event.touches[0].clientY;
-            };
-    
-            const handleTouchMove = (event) => {
-                if (!isTouching) return;
-                event.preventDefault();
-    
-                const currentTouchY = event.touches[0].clientY;
-                const delta = (startTouchY - currentTouchY) * sensitivity;
-    
-                container.scrollBy({
-                    top: delta,
-                    behavior: 'smooth',
-                });
-    
-                startTouchY = currentTouchY;
-            };
-    
-            const handleTouchEnd = () => {
-                isTouching = false;
-            };
-    
-            const handleWheel = (event) => {
-                event.preventDefault();
-                const delta = event.deltaY;
-    
-                container.scrollBy({
-                    top: delta,
-                    behavior: 'smooth',
-                });
-            };
-    
-            container.addEventListener('touchstart', handleTouchStart);
-            container.addEventListener('touchmove', handleTouchMove);
-            container.addEventListener('touchend', handleTouchEnd);
-            container.addEventListener('wheel', handleWheel);
-    
-            return () => {
-                container.removeEventListener('touchstart', handleTouchStart);
-                container.removeEventListener('touchmove', handleTouchMove);
-                container.removeEventListener('touchend', handleTouchEnd);
-                container.removeEventListener('wheel', handleWheel);
-            };
-        };
-    
-        scrollear();
-    }, []);
+  useEffect(() => {
+    const scrollear = () => {
+      const container = document.querySelector(".scroll-container");
 
-    return (
-        <section className='scroll-container'>
-            <Inicio ref={props.refsLista.inicio}></Inicio>
-            <Skills
-                esDispositivoPequeno={esDispositivoPequeno}
-                ref={props.refsLista.skills}
-            ></Skills>
-            {esDispositivoPequeno ? (
-                <SobreMi ref={props.refsLista.sobremi}></SobreMi>
-            ) : null}
+      let isTouching = false;
+      let startTouchY = 0;
+      const sensitivity = 1; // Ajusta la sensibilidad según tus necesidades
 
-            <Proyectos ref={props.refsLista.proyectos}></Proyectos>
-            <Contacto ref={props.refsLista.contacto}></Contacto>
-        </section>
-    );
+      const handleTouchStart = (event) => {
+        isTouching = true;
+        startTouchY = event.touches[0].clientY;
+      };
+
+      const handleTouchMove = (event) => {
+        if (!isTouching) return;
+        event.preventDefault();
+
+        const currentTouchY = event.touches[0].clientY;
+        const delta = (startTouchY - currentTouchY) * sensitivity;
+
+        container.scrollBy({
+          top: delta,
+          behavior: "smooth",
+        });
+
+        startTouchY = currentTouchY;
+      };
+
+      const handleTouchEnd = () => {
+        isTouching = false;
+      };
+
+      const handleWheel = (event) => {
+        event.preventDefault();
+        const delta = event.deltaY;
+
+        container.scrollBy({
+          top: delta,
+          behavior: "smooth",
+        });
+      };
+
+
+      container.addEventListener("touchstart", handleTouchStart, {
+        passive: false,
+      });
+      container.addEventListener("touchmove", handleTouchMove, {
+        passive: false,
+      });
+      container.addEventListener("touchend", handleTouchEnd, {
+        passive: false,
+      });
+      container.addEventListener("touchcancel", handleTouchEnd, {
+        passive: false,
+      });
+      return () => {
+        container.removeEventListener("touchstart", handleTouchStart);
+        container.removeEventListener("touchmove", handleTouchMove);
+        container.removeEventListener("touchend", handleTouchEnd);
+        container.removeEventListener("wheel", handleWheel);
+      };
+    };
+
+    scrollear();
+  }, []);
+
+  return (
+    <section className="scroll-container">
+      <Inicio ref={props.refsLista.inicio}></Inicio>
+      <Skills
+        esDispositivoPequeno={esDispositivoPequeno}
+        ref={props.refsLista.skills}
+      ></Skills>
+      {esDispositivoPequeno ? (
+        <SobreMi ref={props.refsLista.sobremi}></SobreMi>
+      ) : null}
+
+      <Proyectos ref={props.refsLista.proyectos}></Proyectos>
+      <Contacto ref={props.refsLista.contacto}></Contacto>
+    </section>
+  );
 };
 
 export default ContenedorSecciones;
